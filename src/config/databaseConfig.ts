eea27d4a600559e989eba8647dd48ef8ece1c6f9
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-16 14:06:57 -0300

    insert config on database

---
+++ src/config/databaseConfig.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgress',
+    host: 'localhost',
+    username: 'postgress',
+    database: 'sqlnode',
+    password: '@18er0821',
+    define: {
+        timestamps: true,
+        underscored: true
+    },
+};
+
