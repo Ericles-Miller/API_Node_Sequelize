6954e3cec04b26b464628c0020b088eb7df2d6a8
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-16 13:31:12 -0300

    add eslint and prettier on aplication

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
-import express, { Request, Response} from 'express';
-// import { routes } from './routes';
+import  express from "express";
 
 const app = express();
 
-app.get('/', (request: Request, response: Response ) => {
-    return response.json({message: 'World'});
-})
-
-// app.use(routes);
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running!"));
