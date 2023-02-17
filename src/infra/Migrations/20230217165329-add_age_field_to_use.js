e5b89bc65559210d4b8bb32eb95dae42779a893c
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-17 14:05:35 -0300

    insert field on table users to correction the aplication

---
+++ src/infra/Migrations/20230217165329-add_age_field_to_use.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.addColumn(
+      'users', // nome of table
+      'age', // nome of field
+      {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    );
+  },
+  // this field down is execute when the command of migration is execute 
+  async down (queryInterface, Sequelize) {
+    return queryInterface.removeColumn(
+      'users', // nome of table
+      'age'
+  )},
+};
